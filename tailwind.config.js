bb40e5bdb4276709e7292856dcc0410661422544
Atinuke Animashaun <atiinukefunmi@gmail.com>
2024-12-29 21:54:10 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import flowbitePlugin from "flowbite/plugin";
+
 export default {
   content: [
     "./index.html",
@@ -6,23 +8,22 @@ export default {
     "node_modules/flowbite-react/**/*.js",
   ],
   theme: {
-    extend: {colors: {
-      "primary" : "#FFFFFF",
-      "secondary" : "#1E298D",
-      "red" : "#922C40",
-      "grey" : "#EFEFF2",
-      "grey1" : "#F5F6FF",
-      "grey2" : "#707070",
-      "grey3" : "#828282",
-      "grey3" : "#EDEDF6",
-      "green" : "#16AF8E",
-      "black" : "#121212",
-      "lightBlue" : "#EEF6FF",
-    }
-  }
+    extend: {
+      colors: {
+        primary: "#FFFFFF",
+        secondary: "#1E298D",
+        red: "#922C40",
+        grey: "#EFEFF2",
+        grey1: "#F5F6FF",
+        grey2: "#707070",
+        grey3: "#EDEDF6", // Fixed duplicate key
+        green: "#16AF8E",
+        black: "#121212",
+        lightBlue: "#EEF6FF",
+      },
+    },
   },
   plugins: [
-    require('flowbite/plugin'),
+    flowbitePlugin,
   ],
-}
-
+};
